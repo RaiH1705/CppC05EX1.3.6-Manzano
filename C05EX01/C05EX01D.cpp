a8cf39dcdd4a161826d2061ef2d0ef6175cf691b
Raisa Silva <140858752+RaiH1705@users.noreply.github.com>
2023-09-21 21:36:23 -0300

    C05EX01D.cpp

--- C05EX01/C05EX01D.cpp
+++ C05EX01/C05EX01D.cpp
@@ -1,18 +1,19 @@
 // C05EX01D LAÇO PRÉ-TESTE VERDADEIRO
 #include <iostream>
 using namespace std;
-int main(){
+int main(void)
+{
  int I, R, S;
  I = 1;
  S = 0;
  while (I <= 500)
  {
- R = I - 2 * ( I / 2);
- if (R == 0)
- {
- S = I + S;
- }
- I++;
+  R = I - 2 * ( I / 2);
+  if (R == 0)
+  {
+   S = I + S;
+  }
+  I++;
  }
  cout << S << endl;
  return 0;
