f9bd221fdecd3f4524a8516c43bedae9991aef6a
Raisa Silva <140858752+RaiH1705@users.noreply.github.com>
2023-09-21 21:26:48 -0300

    C05EX01B.cpp

---
+++ C05EX01/C05EX01B.cpp
@@ -0,0 +1,18 @@
+// C05EX01B LAÇO PRÉ-TESTE VERDADEIRO
+#include <iostream>
+using namespace std;
+int main (void)
+{
+ int N, T, R;
+ cout << "Entre o valor da tabuada: "; cin >> N;
+ cin.ignore (80, '\n');
+ cout << "\n";
+ T = 1;
+ while (T <= 10)
+ {
+ R = T * N;
+ cout << T << "x" << N << "=" << R << endl;
+ T = T + 1;
+ }
+ return 0;
+}
