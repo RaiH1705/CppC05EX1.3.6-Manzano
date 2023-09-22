6e860fc7c121716ffe1af26783341993bf7a03bd
Raisa Silva <140858752+RaiH1705@users.noreply.github.com>
2023-09-21 21:26:05 -0300

    C05EX01A.cpp

---
+++ C05EX01/C05EX01A.cpp
@@ -0,0 +1,17 @@
+// C05EX01A LAÇO PRÉ-TESTE VERDADEIRO
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (void)
+{
+ int I, Q;
+ I = 15;
+ while (I <= 200)
+ {
+ Q = pow(I,2);
+ cout << Q << endl;
+ I = I + 1;
+ }
+
+ return 0;
+}
