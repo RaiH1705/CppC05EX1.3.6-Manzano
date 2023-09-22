519e4d61515b9a34b81dca2edf571f018709d03a
Raisa Silva <140858752+RaiH1705@users.noreply.github.com>
2023-09-21 21:29:54 -0300

    C05EX01F.cpp

---
+++ C05EX01/C05EX01F.cpp
@@ -0,0 +1,16 @@
+// C05EX01F LAÇO PRÉ-TESTE VERDADEIRO
+#include <iostream>
+using namespace std;
+int main (void)
+{
+ int I, R;
+ I = 1;
+ while (I <= 199)
+ {
+ R = I % 4;
+ if (R == 0)
+ cout << I << endl;
+ I++;
+ }
+ return 0;
+}
