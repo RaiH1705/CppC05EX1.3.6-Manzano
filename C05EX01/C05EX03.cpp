7d21747562d0bfd573a047a313170209f3b06292
Raisa Silva <140858752+RaiH1705@users.noreply.github.com>
2023-09-21 21:27:49 -0300

    C05EX03.cpp

---
+++ C05EX01/C05EX03.cpp
@@ -0,0 +1,17 @@
+// C05EX01C LAÇO PRÉ-TESTE VERDADEIRO
+#include <iostream>
+using namespace std;
+int main (void)
+{
+ int S, I;
+ S = 0;
+ I = 1;
+ while (I <= 100)
+ {
+ S = I + S;
+ I++;
+ }
+ cout << "A somatoria é: " << S << endl;
+
+ return 0;
+}
